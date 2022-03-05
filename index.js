eeb446a2a7eb6c997b8379839820f1adb58dbf03
N44 <nandhu90040@gmail.com>
2022-03-05 10:28:35 +0530

    Modified some bugs.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports.randomColor = require('./hexgen.js')
+const chalk = require('chalk');
+
+module.exports = {
+    randomColor(log=false)  {
+    log = log ?? false;
+     let range = "0123456789abcdef"
+     let colorString = "#"
+     for(let color=0;color<6;color++){
+         colorString += range[Math.floor(Math.random() * range.length)]
+     }
+     if(log) console.log(`[ Hex-Gen ] : `+ chalk.hex(`${colorString}`)(`${colorString}`)) 
+     return colorString
+    }
+}
